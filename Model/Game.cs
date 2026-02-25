00c7a1ffca19a3161d7e295ce07803e8cee50db7
gizul25 <gizul25@student.sdu.dk>
2026-02-25 13:54:20 +0000

    render grid from data

--- Model/Game.cs
+++ Model/Game.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using SquareGame.Infrastructure;
 
 namespace SquareGame.Model;
@@ -6,9 +7,16 @@ public class Game : PropertyChangedBase
 {
     public Game()
     {
+        var square = new Square(0, 0, "Blue");
+        var square2 = new Square(1, 1, "Red");
+        var square3 = new Square(2, 2, "Aqua");
+        SquareList.Add(square);
+        SquareList.Add(square2);
+        SquareList.Add(square3);
     }
 
     public static Game DesignInstance { get; } = new();
 
     public string StatusMessage { get; } = "Blue turn";
+    public ObservableCollection<Square> SquareList { get; } = new();
 }
